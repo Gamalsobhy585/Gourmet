72d7a36b8ff6584dd4cdd6e3b90c902404799450
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-03-29 01:12:18 +0200

    create migrations

--- database/migrations/2025_03_28_230945_create_products_table.php
+++ database/migrations/2025_03_28_230945_create_products_table.php
@@ -6,21 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->string('SKU')->unique();
+            $table->text('description')->nullable();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
