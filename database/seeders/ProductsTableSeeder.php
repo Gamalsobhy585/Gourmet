dd0e4143da4d57d21c0c38c1bef4aa978c31d864
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-03-30 00:45:10 +0200

    update seeder

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -13,35 +13,35 @@ class ProductsTableSeeder extends Seeder
             // Category One Products
             [
                 'name' => 'Product 1A',
-                'price' => 19.99,
+                'price' => 10,
                 'SKU' => 'CAT1-001',
                 'description' => 'Description for Product 1A',
                 'category_id' => 1
             ],
             [
                 'name' => 'Product 2A',
-                'price' => 29.99,
+                'price' => 30,
                 'SKU' => 'CAT1-002',
                 'description' => 'Description for Product 2A',
                 'category_id' => 1
             ],
             [
                 'name' => 'Product 3A',
-                'price' => 39.99,
+                'price' => 40,
                 'SKU' => 'CAT1-003',
                 'description' => 'Description for Product 3A',
                 'category_id' => 1
             ],
             [
                 'name' => 'Product 4A',
-                'price' => 49.99,
+                'price' => 50,
                 'SKU' => 'CAT1-004',
                 'description' => 'Description for Product 4A',
                 'category_id' => 1
             ],
             [
                 'name' => 'Product 5A',
-                'price' => 59.99,
+                'price' => 60,
                 'SKU' => 'CAT1-005',
                 'description' => 'Description for Product 5A',
                 'category_id' => 1
@@ -50,35 +50,35 @@ class ProductsTableSeeder extends Seeder
             // Category Two Products
             [
                 'name' => 'Product 1B',
-                'price' => 24.99,
+                'price' => 25,
                 'SKU' => 'CAT2-001',
                 'description' => 'Description for Product 1B',
                 'category_id' => 2
             ],
             [
                 'name' => 'Product 2B',
-                'price' => 34.99,
+                'price' => 30,
                 'SKU' => 'CAT2-002',
                 'description' => 'Description for Product 2B',
                 'category_id' => 2
             ],
             [
                 'name' => 'Product 3B',
-                'price' => 44.99,
+                'price' => 45,
                 'SKU' => 'CAT2-003',
                 'description' => 'Description for Product 3B',
                 'category_id' => 2
             ],
             [
                 'name' => 'Product 4B',
-                'price' => 54.99,
+                'price' => 55,
                 'SKU' => 'CAT2-004',
                 'description' => 'Description for Product 4B',
                 'category_id' => 2
             ],
             [
                 'name' => 'Product 5B',
-                'price' => 64.99,
+                'price' => 65,
                 'SKU' => 'CAT2-005',
                 'description' => 'Description for Product 5B',
                 'category_id' => 2
@@ -87,35 +87,35 @@ class ProductsTableSeeder extends Seeder
             // Category Three Products
             [
                 'name' => 'Product 1C',
-                'price' => 27.99,
+                'price' => 28,
                 'SKU' => 'CAT3-001',
                 'description' => 'Description for Product 1C',
                 'category_id' => 3
             ],
             [
                 'name' => 'Product 2C',
-                'price' => 37.99,
+                'price' => 38,
                 'SKU' => 'CAT3-002',
                 'description' => 'Description for Product 2C',
                 'category_id' => 3
             ],
             [
                 'name' => 'Product 3C',
-                'price' => 47.99,
+                'price' => 48,
                 'SKU' => 'CAT3-003',
                 'description' => 'Description for Product 3C',
                 'category_id' => 3
             ],
             [
                 'name' => 'Product 4C',
-                'price' => 57.99,
+                'price' => 58,
                 'SKU' => 'CAT3-004',
                 'description' => 'Description for Product 4C',
                 'category_id' => 3
             ],
             [
                 'name' => 'Product 5C',
-                'price' => 67.99,
+                'price' => 68,
                 'SKU' => 'CAT3-005',
                 'description' => 'Description for Product 5C',
                 'category_id' => 3
