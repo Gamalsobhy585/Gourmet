d11a1a04ed4b74b53baf324c97eae90c0691c4b5
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-03-29 14:13:58 +0200

    create messages for ar and en

---
+++ resources/lang/ar/messages.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'product' => [
+        "create_success" => "تم إنشاء المنتج بنجاح.",
+        "create_failed" => "فشل في إنشاء المنتج.",
+        "update_success" => "تم تحديث المنتج بنجاح.",
+        "update_failed" => "فشل في تحديث المنتج.",
+        "delete_success" => "تم حذف المنتج بنجاح.",
+        "delete_failed" => "فشل في حذف المنتج.",
+        "index_success" => "تم استرجاع المنتجات بنجاح."
+    ],
+    'category' => [
+        "create_success" => "تم إنشاء الفئة بنجاح.",
+        "create_failed" => "فشل في إنشاء الفئة.",
+        "update_success" => "تم تحديث الفئة بنجاح.",
+        "update_failed" => "فشل في تحديث الفئة.",
+        "delete_success" => "تم حذف الفئة بنجاح.",
+        "delete_failed" => "فشل في حذف الفئة.",
+        "index_success" => "تم استرجاع الفئات بنجاح."
+    ]
+];
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'product' => [
+            "create_success" => "Product created successfully.",
+            "create_failed" => "Failed to create the product.",
+            "update_success" => "Product updated successfully.",
+            "update_failed" => "Failed to update the product.",
+            "delete_success" => "Product deleted successfully.",
+            "delete_failed" => "Failed to delete the product.",
+            "index_success" => "Products retrieved successfully."
+    ],
+    'category' => [
+            "create_success" => "Category created successfully.",
+            "create_failed" => "Failed to create the category.",
+            "update_success" => "Category updated successfully.",
+            "update_failed" => "Failed to update the category.",
+            "delete_success" => "Category deleted successfully.",
+            "delete_failed" => "Failed to delete the category.",
+            "index_success" => "Categories retrieved successfully."
+    ]
+
+];
